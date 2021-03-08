accb85f482f46f0698ecf9a83894d9344eb93365
Laura Tejada <laura_mtc@hotmail.com>
2021-03-07 23:22:18 -0500

    added checkout steps logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+
+import AppContext from '../context/AppContext';
 
 import '../styles/components/Header.css';
 
-const Header = () => (
-  <div className="Header">
-    <h1 className="Header-title">
-      <Link to="/">Platzi Conf Merch</Link>
-    </h1>
-    <div className="Header-checkout">
-      <Link to="/checkout">
-        <i className="fas fa-shopping-basket" title="Checkout" />
-      </Link>
+const Header = () => {
+  const { state } = useContext(AppContext);
+  const { cart } = state;
+
+  return (
+    <div className="Header">
+      <h1 className="Header-title">
+        <Link to="/">Platzi Conf Merch</Link>
+      </h1>
+      <div className="Header-checkout">
+        <Link to="/checkout">
+          <i className="fas fa-shopping-basket" title="Checkout" />
+        </Link>
+        {cart.length > 0 && <div className="Header-alert">{cart.length}</div>}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+
+import AppContext from '../context/AppContext';
 
 import '../styles/components/Checkout.css';
 
-const Checkout = () => (
-  <div className="Checkout">
-    <div className="Checkout-content">
-      <h3>Lista de Pedidos:</h3>
-      <div className="Checkout-item">
-        <div className="Checkout-element">
-          <h4>ITEM name</h4>
-          <span>$10</span>
-        </div>
-        <button type="button">
-          <i className="fas fa-trash-alt" title="Eliminar" />
-        </button>
+const Checkout = () => {
+  const { state, removeFromCart } = useContext(AppContext);
+  const { cart } = state;
+
+  const handleRemove = product => () => {
+    removeFromCart(product);
+  };
+
+  const handleSumTotal = () => {
+    const reducer = (accumulator, currentItem) => accumulator + currentItem.price;
+    return cart.reduce(reducer, 0);
+  };
+
+  return (
+    <div className="Checkout">
+      <div className="Checkout-content">
+        {cart.length > 0 ? <h3>Lista de Pedidos:</h3> : <h3>Sin pedidos...</h3>}
+        {cart.map(item => (
+          <div className="Checkout-item" key={item.id}>
+            <div className="Checkout-element">
+              <h4>{item.title}</h4>
+              <span>{item.price}</span>
+            </div>
+            <button type="button" onClick={handleRemove(item)}>
+              <i className="fas fa-trash-alt" title="Eliminar" />
+            </button>
+          </div>
+        ))}
       </div>
+      {cart.length > 0 && (
+        <div className="Checkout-sidebar">
+          <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
+          <Link to="/checkout/information">
+            <button type="button">Continuar pedido</button>
+          </Link>
+        </div>
+      )}
     </div>
-    <div className="Checkout-sidebar">
-      <h3>Precio Total: $10</h3>
-      <Link to="/checkout/information">
-        <button type="button">Continuar pedido</button>
-      </Link>
-    </div>
-  </div>
-);
+  );
+}
 
 export default Checkout;
